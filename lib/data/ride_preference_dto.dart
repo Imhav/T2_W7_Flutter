646ba63474d9a5dc1b6358ccb1be0e3cf23fea66
Imhav <ouvenhav500@gmail.com>
2025-04-12 19:31:00 +0700

    "BLA-204 – Implement the Local Storage repository."

---
+++ lib/data/ride_preference_dto.dart
@@ -0,0 +1,23 @@
+
+import 'package:week_3_blabla_project/model/ride/ride_pref.dart';
+
+import '../../data/location_dto.dart';
+
+class RidePreferenceDto {
+  static Map<String, dynamic> toJson(RidePreference model) {
+    return {
+      'arrival': LocationDto.toJson(model.arrival),
+      'departure': LocationDto.toJson(model.departure),
+      'departureDate': model.departureDate.toIso8601String(),
+      'requestedSeats': model.requestedSeats
+    };
+  }
+
+  static RidePreference fromJson(Map<String, dynamic> json) {
+    return RidePreference(
+        arrival: LocationDto.fromJson(json['arrival']),
+        departure: LocationDto.fromJson(json['departure']),
+        departureDate: DateTime.parse(json['departureDate']),
+        requestedSeats: json['requestedSeats']);
+  }
+}
